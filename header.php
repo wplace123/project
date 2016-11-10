5efde5b15f2ef5d4ba1c92f4673b37ff8c80b088
wplace123 <wplace123@gmail.com>
2016-11-10 10:21:52 +0800

    第一次測試

---
+++ header.php
@@ -0,0 +1,42 @@
+<?php header("Content-Type:text/html; charset=utf8"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- 行動裝置支援 -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<style type="text/css">
+	.navbar{
+		background: #555;
+	}
+	.glyphicon{
+		margin-right: 5px;
+	}
+	#nav-content{
+		color: #FFF;
+	}
+	</style>
+</head>
+<body>
+	<nav class="navbar navbar-default" role="navigation">
+	  <div class="container-fluid">
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li>
+	        	<a id="nav-content" href="index.php">會員註冊</a>
+	        </li>
+	        <li>
+	        	<a id="nav-content" href="login.php"><span class="glyphicon glyphicon-log-in"></span>登入</a>
+	        </li>
+	        <li>
+	        	<a id="nav-content" href="login.php"><span class="glyphicon glyphicon-shopping-cart">(0)</span></a>
+	        </li>
+	      </ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</nav>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<?php include_once("header.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+	<style type="text/css">
+	
+	</style>
+</head>
+<body>
+	<div class="container" style="margin-top:100px">
+		<div class="col-md-offset-4 col-md-4">
+	        <form role="form">
+	            <div class="form-group">
+	                <label for="exampleInputEmail1">電子郵件</label>
+	                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="請輸入帳號"> 
+	            </div>
+	            <div class="form-group">
+	                <label for="exampleInputPassword1">密碼</label>
+	                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="請輸入密碼">
+	            </div>
+	     
+	            <button type="submit" style="margin-top:10px" class="btn btn-default"><b>登入</b></button>
+	        </form>
+		</div>
+	</div>
+</body>
+</html>
+	
